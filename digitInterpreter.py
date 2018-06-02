42205079344607cfaabac42f2330d8e93c55aae5
poor-fool-mloody <michalsochacki111@gmail.com>
2018-06-02 17:29:48 +0200

    Added comments in polish language 2.

--- digitInterpreter.py
+++ digitInterpreter.py
@@ -28,38 +28,39 @@ def knn(train_data, test_data, k):
                     output_writer2 = csv.DictWriter(output_file_errors, fieldnames=fieldnames2, delimiter=',')
                     output_writer2.writeheader()
 
-                    for test_digit in test_reader: # pętla zczytująca każdą cyfrę testującą z zestawu cyfr testujących
-                        tmp = []  # temporary list to specify shortest distances between vectors
-                        train_data.seek(0)
+                    for test_digit in test_reader: # pętla sczytująca każdą cyfrę testującą z zestawu cyfr testujących
+                        tmp = []  # tymczasowa lista przechowująca euklidesowe odległości pomiędzy wektorem cyfry testującej od uczącej
+                        train_data.seek(0) # ustawienie wskaźnika aktualnego czytania danych uczącyhc z pliku na początek
 
-                        for train_digit in train_reader: # pętla zczytująca każdą cyfrę uczącą z zestawu cyfr uczących
-                            distance = euclidean_distance(train_digit, test_digit)
-                            tmp.append([train_digit[0], distance])
+                        for train_digit in train_reader: # pętla sczytująca każdą cyfrę uczącą z zestawu cyfr uczących
+                            distance = euclidean_distance(train_digit, test_digit) # lista przechowująca wartość odległości pomiędzy wektorem cyfry testującej od cyfry uczącej
+                            tmp.append([train_digit[0], distance]) # poszerzenie listy o nową odległość
 
-                        classified = classify(tmp, k)
-                        output_writer.writerow(
+                        classified = classify(tmp, k) # lista przechowująca rezultat klasyfikacji dla k=1..k sąsiadów
+                        output_writer.writerow( # wypisywanie sklasyfikowanych cyfr do pliku classified.csv
                             {fieldnames[0]: test_digit[0], fieldnames[1]: classified[0], fieldnames[2]: classified[1],
                              fieldnames[3]: classified[2], fieldnames[4]: classified[3], fieldnames[5]: classified[2],
                              fieldnames[6]: classified[3], fieldnames[7]: classified[3]})
 
-                        statistics(classified, test_digit)  # write additional statistics to another file
+                        errors(classified, test_digit)
 
-    output_writer2.writerow(
+    print(fieldnames2[6])
+    output_writer2.writerow( # wypisywanie błędów klasyfikacyjnych dla k=1..k sąsiadów do pliku errors.csv
         {fieldnames2[0]: error[0], fieldnames2[1]: error[1], fieldnames2[2]: error[2],
          fieldnames2[3]: error[3], fieldnames2[4]: error[4], fieldnames2[5]: error[5],
          fieldnames2[6]: error[6]})
 
 
-def euclidean_distance(vect_1, vect_2):
+def euclidean_distance(vect_1, vect_2): # funkcja obliczająca odległość Euklidesową pomiędzy dwoma zadanymi wektorami
     distance = 0
     for column in range(1, len(vect_1)):
         distance += (int(vect_1[column]) - int(vect_2[column])) ** 2
-
+        print(distance)
     return distance ** .5
 
 
-def classify(distances, k):
-    classification_votes = {}
+def classify(distances, k): # funkcja klasyfikująca cyfrę
+    classification_votes = {} # słownik przechowujący ilość wystąpień takiej samej sklasyfikowanej cyfry dla k=1..k najkrótszych odległości
     sorted_classification_votes_table = []  # is a retured list that contain classified digit for k =1, k =2,.., k = 7
     distances.sort(key=operator.itemgetter(1))
 
@@ -86,16 +87,10 @@ def classify(distances, k):
     '''
 
 
-def statistics(classified, test_digit):
+def errors(classified, test_digit): # funkcja zwiększająca licznik błędów dla k=1..k sąsiadów jeśli sklasyfikowana cyfra różni się od rzeczywistej cyfry
     for x in range(len(classified)):
         if int(classified[x]) != int(test_digit[0]):
             error[x] += 1
-            # error_table[int(test_digit[0])][int(classified)] += 1
-            # print("pomylone ", int(test_digit[0]), int(classified))
-            # for x in range(len(error_table)):
-            #     print(error_table[x])
-            # print("next:")
-            # print(error_table[int(test_digit[0])][int(classified)])
     print("errors:", error)
 
 
