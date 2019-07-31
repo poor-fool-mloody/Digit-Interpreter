918c2d3c67d82dec3a5dc3c585a712e903533543
Michał Sochacki <michalsochacki111@gmail.com>
2019-07-31 16:43:57 +0200

    Code refactor

--- digitInterpreter.py
+++ digitInterpreter.py
@@ -1,58 +1,58 @@
-from multiprocessing import Process
 import csv
 import operator
-import matplotlib.pyplot as plt  # zaimportowanie biblioteki rysującej wykresy
+import matplotlib.pyplot as plt  # library for plotting charts
 
 
 def knn(train_data, test_data, k):
-    output_file_classification = 'classification.csv'  # zestaw danych sklasyfikowanych dla k=1..k
-    output_file_errors = 'errors.csv'  # całkowita ilość popełnionych będów klasyfikacyjnych dla k=1..k
-    output_file_errors2 = 'errors.csv'  # całkowita ilość popełnionych będów klasyfikacyjnych dla k=1..k
+    output_file_classification = 'classification.csv'  # output file with classified digits for k=1..k neighbours respectively
+    output_file_errors = 'errors.csv'  # total amount of bad predictions for k=1..k neighbours respectively
+    output_file_errors2 = 'errors.csv'  # total amount of bad predictions for k=1..k neighbours respectively
 
     error = [0 for x in range(
-        k)]  # lista w której przechowywane są całkowite ilości popełnionych będów klasyfikacyjnych dla k=1..k sąsiadów
+        k)]  # this list hold total amount of bad predictions for k=1..k neighbours respectively
 
-    with open(test_data, 'r') as test_data:  # otwarcie pliku z zestawem danych testujących
+    with open(test_data, 'r') as test_data:  # read test dataset
         test_reader = csv.reader(test_data, delimiter=',')
 
-        with open(train_data, 'r') as train_data:  # otwarcie pliku z zestawem danych uczących
+        with open(train_data, 'r') as train_data:  # read train dataset
             train_reader = csv.reader(train_data, delimiter=',')
 
             with open(output_file_classification, 'w',
-                      newline='') as output_file_classification:  # otwarcie pliku wyjściowego zawierającego zestaw danych sklasyfikowanych dla k=1..k
+                      newline='') as output_file_classification:  # open output file for writing classified info for k=1..k neighbours
                 fieldnames = ['test digit', 'digit clasified for k=1', 'k=2', 'k=3', 'k=4', 'k=5', 'k=6',
-                              'k=7']  # ustalenie nazw kolumn w pliku classification.csv
+                              'k=7']  # define column names
                 output_writer = csv.DictWriter(output_file_classification, fieldnames=fieldnames, delimiter=',')
                 output_writer.writeheader()
 
                 with open(output_file_errors, 'w',
-                          newline='') as output_file_errors:  # otwarcie pliku wyjściowego zawierającego całkowitą iloś popełnionych będów klasyfikacyjnych dla k=1..k sąsiadów
-                    fieldnames2 = ['k', 'number of errors']  # ustalenie nazw kolumn w pliku errors.csv
+                          newline='') as output_file_errors:  # open output file for writing total amount of bad predictions for k=1..k neighbours respectively
+                    fieldnames2 = ['k', 'number of errors']  # define column names
                     output_writer2 = csv.DictWriter(output_file_errors, fieldnames=fieldnames2, delimiter=',')
                     output_writer2.writeheader()
 
-                    for test_digit in test_reader:  # pętla sczytująca każdą cyfrę testującą z zestawu cyfr testujących
-                        tmp = []  # tymczasowa lista przechowująca euklidesowe odległości pomiędzy wektorem cyfry testującej od uczącej
+                    for test_digit in test_reader:  # loop over every record from test dataset
+                        tmp = []  # temporary list containing Euclidean distance between test digit vector and train digit vector
                         train_data.seek(
-                            0)  # ustawienie wskaźnika aktualnego czytania danych uczącyhc z pliku na początek
+                            0)  # set file reading pointer to the beginning of the file
 
-                        for train_digit in train_reader:  # pętla sczytująca każdą cyfrę uczącą z zestawu cyfr uczących
+                        for train_digit in train_reader:  # loop over every record from train dataset
                             distance = euclidean_distance(train_digit,
-                                                          test_digit)  # lista przechowująca wartość cyfry testującej oraz jej wartość odległości pomiędzy wektorem tej cyfry od wektora cyfry uczącej
-                            tmp.append([train_digit[0], distance])  # poszerzenie listy o nową odległość
+                                                          test_digit)  # list that contains test digit value and its distance to train digit
+                            tmp.append([train_digit[0], distance])  # append new distance to tmp list
 
-                        classified = classify(tmp, k)  # lista przechowująca rezultat klasyfikacji dla k=1..k sąsiadów
-                        output_writer.writerow(  # wypisywanie sklasyfikowanych cyfr do pliku classified.csv
+                        classified = classify(tmp,
+                                              k)  # list that contains classification results for k=1..k neighbours respectively
+                        output_writer.writerow(  # write classified digits into classified.csv
                             {fieldnames[0]: test_digit[0], fieldnames[1]: classified[0], fieldnames[2]: classified[1],
                              fieldnames[3]: classified[2], fieldnames[4]: classified[3], fieldnames[5]: classified[2],
                              fieldnames[6]: classified[3], fieldnames[7]: classified[3]})
 
                         errors(classified, test_digit, error)
 
-                    for x in range(k):  # wypisywanie błędów klasyfikacyjnych dla k=1..k sąsiadów do pliku errors.csv
+                    for x in range(k):  # write classification errors to errors.csv for k=1..k neighbours respectively
                         output_writer2.writerow({fieldnames2[0]: x + 1, fieldnames2[1]: error[x]})
 
-    with open(output_file_errors2, 'r') as output_file_errors:  # otwarcie pliku z błędami klasyfikacji
+    with open(output_file_errors2, 'r') as output_file_errors:  # open file containing classification errors for reading
         output_reader = csv.reader(output_file_errors, delimiter=',')
         x = []
         y = []
@@ -63,43 +63,42 @@ def knn(train_data, test_data, k):
             y.append(int(row[1]))
 
         plt.plot(x, y, 'ro')
-        plt.title('Wykres błędów')
-        plt.xlabel('ilość sąsiadów')
-        plt.ylabel('liczba błędów klasyfikacji')
-        plt.show()  # funkcja rysująca wykres
+        plt.title('Error chart')
+        plt.xlabel('number of neighbours')
+        plt.ylabel('total amount of bad predictions')
+        plt.show()
 
 
-def euclidean_distance(vect_1, vect_2):  # funkcja obliczająca odległość Euklidesową pomiędzy dwoma zadanymi wektorami
+def euclidean_distance(vect_1, vect_2):  # utility function computing Euclidean distance between two given vectors
     distance = 0
     for column in range(1, len(vect_1)):
         distance += (int(vect_1[column]) - int(vect_2[column])) ** 2
     return distance ** .5
 
 
-def classify(distances, k):  # funkcja klasyfikująca cyfrę
-    classification_digits = {}  # słownik przechowujący ilość wystąpień takiej samej sklasyfikowanej cyfry dla k=1..k najkrótszych odległości
-    sorted_classification_digits_table = []  # zwracana przez funkcję lista przechowująca rezultat klasyfikacji dla k=1..k sąsiadów
+def classify(distances, k):  # function for predicting what class digit belongs
+    classification_digits = {}  # dictionary that holds number of same digit occurence for k=1..k shortest distances
+    sorted_classification_digits_table = []
     distances.sort(key=operator.itemgetter(
-        1))  # sortowanie (od najmniejszej wartości) listy przechowującej odległości pomiędzy cyfrą testującą a uczącą
+        1))
 
-    for x in range(k):  # pętla iterująca od 1 do k odległości w liście distances
+    for x in range(k):
         digit = distances[x][0]
-        if digit in classification_digits:  # jeśli zadana cyfra wystąpiła już w x najkrótszych odległościach to zwiększ licznik wystąpień danej cyfry
+        if digit in classification_digits:  # if digit appeared in x shortest distance then increase appearence counter of this digit
             classification_digits[digit] += 1
-        else:  # w przeciwnym wypadku ustaw licznik wystąpień zadanej cyfry na 1
+        else:  # set digit occurrence to 1
             classification_digits[digit] = 1
         sorted_classification_digits = sorted(classification_digits.items(), key=operator.itemgetter(1),
-                                              reverse=True)  # posortowanie słownika (od największej wartości) w zależności od liczby wystąpień klasyfikacji danej cyfry
+                                              reverse=True)  # sort dictionary descending
         sorted_classification_digits_table.append(sorted_classification_digits[0][
-                                                      0])  # weź pod uwagę cyfrę sklasyfikowaną która ma największą ilość wystąpień w słowniku
+                                                      0])
 
     return sorted_classification_digits_table
 
 
-
 def errors(classified,
            test_digit,
-           error):  # funkcja zwiększająca licznik błędów dla k=1..k sąsiadów jeśli sklasyfikowana cyfra różni się od rzeczywistej cyfry
+           error):  # function that increase amount of bad predictions for k=1..k neighbours respectively
     for x in range(len(classified)):
         if int(classified[x]) != int(test_digit[0]):
             error[x] += 1
@@ -107,17 +106,12 @@ def errors(classified,
 
 
 def main():
-    train_data = 'mnist_train_train.csv'  # zestaw danych uczących
-    test_data = 'mnist_test3.csv'  # zestaw danych testujących
-    k = 7  # liczba sąsiadów
+    train_data = 'mnist_train.csv'  # train dataset
+    test_data = 'mnist_test.csv'  # test dataset
+    k = 7  # number of neighbours
 
     knn(train_data, test_data,
-        k)  # knn - funkcja klasyfikująca algorytmem k najbliższych sąsiadów zadane cyfry arabskie z zestawu testujących danych MNIST na podstawie uczącego zestawu danych MNIST.
-    #  Tworzy 2 pliki: classification.csv oraz errors.csv. classification.csv zawiera zestaw cyfr oraz odpowiadające im te cyfry sklasyfikowane przez algorytm dla k=1..k sąsiadów. errors.csv zawiera całkowitą liczbę popełnionych błędów klasyfikacji dla k=1..k sąsiadów.
-
-    # p2 = Process(target=, args=())
-    # p3 = Process(target=, args=())
-    # p4 = Process(target=, args=())
+        k)  # run prediction
 
 
 if __name__ == '__main__':
